a77adc8fcbd779427938fb5ce473dc9a478c6007
Yuan Yizhe <yuanyizhe@gmail.com>
2016-12-01 14:14:28 +0800

    move command execute to command class

--- app/core/fs/command.js
+++ app/core/fs/command.js
@@ -2,6 +2,14 @@ export default class FSCommand {
   constructor() {
   }
 
-  
+  /**
+   * Execute Command
+   * @param {Object} command - Command Object
+   * @param {string} command.method - Command Method
+   * @param {string[]} command.args - Command Args Array
+   */
+  execute(command) {
+
+  } 
 
 }
