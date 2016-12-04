dadcf403b65777dfadcb700afadeafb28acc59a4
Yuan Yizhe <yuanyizhe@gmail.com>
2016-12-04 20:25:26 +0800

    update unix fs

--- app/core/fs/unix.js
+++ app/core/fs/unix.js
@@ -8,6 +8,14 @@ export default class FSUnix extends FSInterface {
     this._type = 'unix'
   }
 
+  writeFile(path, data, callback) {
+    fs.writeFile(path, data, callback)
+  }
+
+  readFile(path, callback) {
+    fs.readFile(path, callback)
+  }
+
   readDir(path, callback) {
     fs.readdir(path, callback)
   }
@@ -18,20 +26,48 @@ export default class FSUnix extends FSInterface {
         callback(err, null)
         return
       }
-      var type = 'other'
-      if (stats.isFile()) {
-        type = 'file'
-      } else if (stats.isDirectory) {
-        type = 'directory'
-      }
+      stats.isDirectory()
       callback(null, {
-        type: type,
+        type: this._resolveFileType(stats.mode),
         size: stats.size,
         mtime: stats.mtime,
         ctime: stats.ctime,
         stats: stats
       })
     })
+  }
+
+  rename(oldPath, newPath, callback) {
+    fs.rename(oldPath, newPath, callback)
+  }
+
+  remove(path, callback) {
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        callback(err, null)
+        return
+      }
+      if (stats.isFile()) {
+        fs.unlink(path, callback)
+      } else if (stats.isDirectory()) {
+        fs.rmdir(path, callback)
+      }
+    })
+  }
+
+  reloadDir(path, callback) {
+    this.readDir(path, callback)
+  }
+
+  _resolveFileType(mode) {
+    // TODO implement all file type
+    switch (mode & fs.constants.S_IFMT) {
+      case fs.constants.S_IFREG:
+        return '文件'
+      case fs.constants.S_IFDIR:
+        return '文件夹'
+    }
+    return '其他'
   }
 
 }
