ded9683cf417d15cd4556ac02dc2b2c521131718
Yuan Yizhe <yuanyizhe@gmail.com>
2016-12-14 04:27:51 +0800

    add readDir and stat

--- app/core/fs/unix.js
+++ app/core/fs/unix.js
@@ -8,8 +8,30 @@ export default class FSUnix extends FSInterface {
     this._type = 'unix'
   }
 
-  readDir(path) {
-    return fs.readdirSync(path)
+  readDir(path, callback) {
+    fs.readdir(path, callback)
+  }
+
+  stat(path, callback) {
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        callback(err, null)
+        return
+      }
+      var type = 'other'
+      if (stats.isFile()) {
+        type = 'file'
+      } else if (stats.isDirectory) {
+        type = 'directory'
+      }
+      callback(null, {
+        type: type,
+        size: stats.size,
+        mtime: stats.mtime,
+        ctime: stats.ctime,
+        stats: stats
+      })
+    })
   }
 
 }
